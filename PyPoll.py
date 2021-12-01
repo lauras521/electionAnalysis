c29bb1c97cf2b3bb26bc59b05fa44756e7c98ad1
lauras521 <laurastock521@gmail.com>
2021-12-01 15:14:07 -0600

    updated code to determine winner

--- PyPoll.py
+++ PyPoll.py
@@ -8,17 +8,58 @@ import os
 file_to_load=os.path.join("Resources","election_results.csv")
 #assign variable to save election results csv to a path
 file_to_save=os.path.join("Resources","election_results.csv")
+
+#initialize total votes to 0
+total_votes=0
+
+#delcare candidate options as an open list
+candidate_options=[]
+
+#declare open dictionary to help tally votes by candidate
+candidate_votes={}
+
 #open election results and read the file
 with open(file_to_load) as election_data:
     #To do: read and analayze data
     #read the file object with the reader function
     file_reader=csv.reader(election_data)
-    #skip first row heading
+
+    #read the header row
     headers=next(file_reader)
-    print(headers)
-    # #print each row in the csv file
-    # for row in file_reader:
-    #     print(row[0])
+    #print(headers)
+    #print each row in the csv file
+    for row in file_reader:
+        #print(row)
+        #add to the total vote count
+        total_votes +=1
+        #print candidate name for each row
+        candidate_name=row[2]
+
+        #if candidate doesn't match an existing candidate in the list
+        if candidate_name  not in candidate_options:
+            #add the candidate to the list
+            candidate_options.append(candidate_name)
+            #begin tracking candidates vote count, set it to zero so we can begin tracking
+            candidate_votes[candidate_name]=0
+        candidate_votes[candidate_name]+=1
+#print candidate list
+print(candidate_options)
+#print candidate votes
+print(candidate_votes)
+
+#itermate through candidate list
+#retrieive vote count of candidate
+
+#calculate percentage of votes
+
+#print the candidate name and percentage of votes received
+for i in candidate_votes:
+    votes=candidate_votes[i]
+    vote_percentage=float(votes)/float(total_votes)*100
+    print(f"{i}: received {vote_percentage:.1f} % of the vote.")
+#print(total_votes)
+
+
 
 
 
